5ae6fac0f51d2cff910f8aadf32997bc6e62f6a2
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-06-08 17:15:43 +0900

    リスト項目を作成

--- widgetbook/lib/page/user_list/component/user_list_item.dart
+++ widgetbook/lib/page/user_list/component/user_list_item.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_train_2025/domain/entity/user.dart';
 import 'package:flutter_train_2025/page/user_list/component/user_list_item.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'UserListItem', type: UserListItem)
 Widget buildUserListUseCase(BuildContext context) {
-  return const UserListItem(name: 'John Doe');
+  final user = User(
+    id: 7,
+    email: "michael.lawson@reqres.in",
+    firstName: "Michael",
+    lastName: "Lawson",
+    avatarImageUrl: "https://reqres.in/img/faces/7-image.jpg",
+  );
+  return Column(children: [UserListItem(user: user)]);
 }
