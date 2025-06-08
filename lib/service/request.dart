1c653efcb73d18cd6dbc4c4f2a0b184b108e0535
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-06-08 20:17:03 +0900

    dio 追加 + 低レベルリクエスト関数作成。

---
+++ lib/service/request.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+
+final _dio = Dio();
+
+Future<Map<String, dynamic>> request({
+  required String method,
+  required String path,
+  Map<String, String> headers = const {},
+  Map<String, String> queryParameters = const {},
+}) async {
+  final baseUrl = 'https://reqres.in/api';
+  final response = await _dio.get(
+    '$baseUrl/$path',
+    queryParameters: queryParameters,
+  );
+  return jsonDecode(response.data);
+}
