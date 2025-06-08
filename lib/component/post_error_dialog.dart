40a419ea995a51d643fcc11fe6e3a106ecfc538d
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-06-08 19:06:41 +0900

    コード簡略化

--- lib/component/post_error_dialog.dart
+++ lib/component/post_error_dialog.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PostErrorDialog extends StatelessWidget {
-  const PostErrorDialog({super.key, required this.onOk});
-
-  final VoidCallback onOk;
+  const PostErrorDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +9,14 @@ class PostErrorDialog extends StatelessWidget {
     return AlertDialog(
       title: Text('エラー', style: theme.textTheme.titleLarge),
       content: Text('ネットワークエラーが発生しました。', style: theme.textTheme.bodyMedium),
-      actions: [TextButton(onPressed: onOk, child: const Text('OK'))],
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('OK'),
+        ),
+      ],
     );
   }
 }
--- widgetbook/lib/component/post_error_dialog.dart
+++ widgetbook/lib/component/post_error_dialog.dart
@@ -4,27 +4,12 @@ import 'package:flutter_train_2025/component/post_error_dialog.dart';
 
 @widgetbook.UseCase(name: 'PostErrorDialog', type: PostErrorDialog)
 Widget buildPostErrorDialogUseCase(BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Builder(
-          builder:
-              (context) => ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder:
-                        (context) => PostErrorDialog(
-                          onOk: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                  );
-                },
-                child: const Text('投稿時エラーダイアログを表示'),
-              ),
-        ),
-      ),
+  return Center(
+    child: ElevatedButton(
+      onPressed: () {
+        showDialog(context: context, builder: (context) => PostErrorDialog());
+      },
+      child: const Text('投稿時エラーダイアログを表示'),
     ),
   );
 }
