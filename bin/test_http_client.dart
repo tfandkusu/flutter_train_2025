caf7161c827d871dafd9f6111c268fbd984c06d6
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-06-08 21:31:42 +0900

    ドメイン層を作成

--- bin/test_http_client.dart
+++ bin/test_http_client.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: avoid_print
 
-import 'package:flutter_train_2025/service/get_users/service.dart';
+import 'package:flutter_train_2025/domain/user/get_users.dart';
 
 void main(List<String> arguments) async {
-  final getUsersDto = await getUsersService(1);
-  print(getUsersDto);
+  final users = await getUsers(1);
+  print(users);
 }
--- lib/domain/entity/user.dart
+++ lib/domain/entity/user.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_train_2025/service/get_users/get_users_dto.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user.freezed.dart';
@@ -11,4 +12,14 @@ abstract class User with _$User {
     required String lastName,
     required String avatarImageUrl,
   }) = _User;
+
+  factory User.fromDto(UserDto dto) {
+    return User(
+      id: dto.id,
+      email: dto.email,
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      avatarImageUrl: dto.avatar,
+    );
+  }
 }
